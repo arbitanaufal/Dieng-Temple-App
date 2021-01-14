0b5d2bc4e2dc67323256ac04d6d6333576867add
arbitanaufal <arbitanaufal@gmail.com>
2021-01-14 22:00:50 +0700

    Fix Camera Movement

--- DTA/Assets/Scripts/CameraMovement.cs
+++ DTA/Assets/Scripts/CameraMovement.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] private Camera cam;
+    [SerializeField] private Camera cam; //Cam = Jenis object //cam = nama object
+    [SerializeField] private Transform target; //Transform = Jenis Object //target = nama object
 
-    private Vector3 previousPosition;
-    // Update is called once per frame
-    void Update()
+    private Vector3 oldPosition; //private = variable //Vector3 = jenis variable //oldPosition = nama variable
+
+    void Update()//Update berlangsung setiap saat terjadi perubahan
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //Input.GetMouseButtonDown = Ditekan // (0) = Left click (1) = Right click
         {
-            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+            oldPosition = cam.ScreenToViewportPoint(Input.mousePosition); //Posisi vector3(x,y,z) = kamera sesuai point tekanan jari menurut Viewport Point 
         }
-        
-        if (Input.GetMouseButton(0))
+        else if (Input.GetMouseButton(0))
         {
-            Vector3 direction = previousPosition - cam.ScreenToViewportPoint(Input.mousePosition);
+            Vector3 newPosition = cam.ScreenToViewportPoint(Input.mousePosition); //newPosition = nama variable posisi baru //newPosition = kamera sesuai point tekanan jari menurut Viewport Point
+            Vector3 direction = oldPosition - newPosition; //arah Vector3 = oldPosition - newPosition
 
-            cam.transform.RotateAround(new Vector3(), new Vector3(0, 1, 0), direction.y * 180);
+            float rotationAroundYAxis = -direction.x * 180; 
+
+            cam.transform.position = target.position;
+
+            cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World);
+
+            cam.transform.Translate(new Vector3(0, 0, 0));
+
+            oldPosition = newPosition;
         }
     }
 }
