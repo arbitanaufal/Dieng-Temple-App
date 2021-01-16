61d0b8f534cff26c9eca33381d8c8244e3a901f7
arbitanaufal <arbitanaufal@gmail.com>
2021-01-16 20:27:44 +0700

    Movement System

--- DTA/Assets/Scripts/MoveSystem.cs
+++ DTA/Assets/Scripts/MoveSystem.cs
@@ -7,20 +7,30 @@ public class MoveSystem : MonoBehaviour
     public GameObject correctForm;
     public GameObject CameraMovement;
     private bool moving;
+    private bool finish;
 
     private float startPosX;
     private float startPosY;
     private float startPosZ;
 
+    private Vector3 resetPosition;
+
+    void Start()
+    {
+        resetPosition = this.transform.localPosition;
+    }
     void Update()
     {
-        if (moving)
+        if (finish == false)
         {
-            Vector3 mousePos;
-            mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            if (moving)
+            {
+                Vector3 mousePos;
+                mousePos = Input.mousePosition;
+                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
-            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, mousePos.z - startPosZ);
+                this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, mousePos.z - startPosZ);
+            }
         }
     }
 
@@ -45,5 +55,17 @@ public class MoveSystem : MonoBehaviour
     {
         moving = false;
         CameraMovement.SetActive(true);
+
+        if (Mathf.Abs(this.transform.localPosition.x - correctForm.transform.localPosition.x) <= 0.5f &&
+            Mathf.Abs(this.transform.localPosition.y - correctForm.transform.localPosition.y) <= 0.5f &&
+            Mathf.Abs(this.transform.localPosition.z - correctForm.transform.localPosition.z) <= 0.5f)
+        {
+            this.transform.position = new Vector3(correctForm.transform.position.x, correctForm.transform.position.y, correctForm.transform.position.z);
+            finish = true;
+        }
+        else
+        {
+            this.transform.localPosition = new Vector3(resetPosition.x, resetPosition.y, resetPosition.z);
+        }
     }
 }
