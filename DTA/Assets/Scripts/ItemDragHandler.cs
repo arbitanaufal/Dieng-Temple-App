2981bdf1fac71ffa5f56e61f0323a29f2c1356d4
arbitanaufal <arbitanaufal@gmail.com>
2021-01-18 21:57:42 +0700

    DTA Beta 1.1.0

--- DTA/Assets/Scripts/ItemDragHandler.cs
+++ DTA/Assets/Scripts/ItemDragHandler.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ItemDragHandler : MonoBehaviour
+public class ItemDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
 {
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.localPosition = Vector3.zero;
+    }
     // Start is called before the first frame update
     void Start()
     {
